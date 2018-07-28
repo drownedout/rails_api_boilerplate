fdd85d0d751595933dddfbfcb90435d6e7bf9512
drownedout <danielcdonnelly@gmail.com>
2018-07-27 19:33:31 -0700

    Added authorization method to applicaiton controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include Response
   include ExceptionHandler
+
+  # Uncomment below to force authorization before requests
+
+  #before_action :authorize_request
+  #attr_reader :current_user
+
+  private
+
+  def authorize_request
+  	@current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  end
+
 end
