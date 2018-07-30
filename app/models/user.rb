9910cea67127bad54e2dd68ff032fb0002165213
drownedout <danielcdonnelly@gmail.com>
2018-07-30 13:07:30 -0700

    Additional validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
 	
 	# validations
 	validates_presence_of :first_name, :email, :password_digest
+	validates_uniqueness_of :email, case_sensative: false
+	validates_format_of :email, with: /@/
+
+	before_save :email_to_downcase
+
+	def email_to_downcase
+	  self.email = self.email.delete(' ').downcase
+	end
 end
